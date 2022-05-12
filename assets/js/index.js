f5ddab22e979cb389c51d4a09147c664793e0082
FusionSid <siddheshadsv@icloud.com>
2022-05-12 17:25:29 +1200

    Cool site now

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,4 @@
-function syncDelay(milliseconds) {
-  var start = new Date().getTime();
-  var end = 0;
-  while (end - start < milliseconds) {
-    end = new Date().getTime();
-  }
-}
-
 window.addEventListener("load", function () {
-  syncDelay(2000);
   window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
   window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
 });
