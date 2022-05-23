c3dd25d9cc19414179f34d0881f6fbce6d4e0242
FusionSid <siddheshadsv@icloud.com>
2022-05-24 10:12:21 +1200

    Creates a new nav bar, I know it looks shit but it wasn't copy pasted out of w3schools this time so its better

--- assets/js/rickroll.js
+++ assets/js/rickroll.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
-  window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
-  window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
-});
+// window.addEventListener("load", function () {
+//   window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+//   window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
+// });
