afc8610f0d3e08d98b7fef235d789f1a332a997b
FusionSid <siddheshadsv@icloud.com>
2022-05-30 11:38:34 +1200

    Added toggleable video link, Also updated top divs to include title

--- assets/js/rickroll.js
+++ assets/js/rickroll.js
@@ -1,4 +1,8 @@
-// window.addEventListener("load", function () {
-//   window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
-//   window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
-// });
+window.addEventListener("load", function () {
+  const rickroll = localStorage.getItem('rickroll');
+  if (rickroll !== "yes") {
+    return
+  }
+  window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
+  window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0");
+});
